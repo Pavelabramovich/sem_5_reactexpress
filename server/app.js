37e022661386680e3b74654e8e584000bb609b68
Pavelabramovich <apn20040510@gmail.com>
2023-11-29 07:53:17 +0300

    add first model

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config({ debug: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,35 +15,12 @@ const cors = require('cors');
 
 var app = express();
 
-app.use(cors({origin: 'http://localhost:3001'}));
-
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-
+const origin = process.env.BASE_URL || 'http://localhost:9000';
+app.use(cors({origin: origin}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ server/models/models.js
@@ -0,0 +1,9 @@
+const sequelize = require('../db');
+const {DataTypes, INTEGER} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    login: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: INTEGER, defaultValue: 1}
+})
